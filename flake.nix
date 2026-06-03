87456a7f7a95dd1e927d646c21f1139f69300252
qaCow37 <daily.cow37@proton.me>
2026-06-03 23:04:27 +0000

    update

--- flake.nix
+++ flake.nix
@@ -2,24 +2,27 @@
 	description = "NixOS Flake for my System";
 	inputs = {
 		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-		home-manager.url = "github:nix-community/home-manager";
+		home-manager = {
+			url = "github:nix-community/home-manager";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
 	};
 
 	outputs = {self, nixpkgs, home-manager, ...}:
 	let
 		system = "x86_64-linux";
+		pkgs = nixpkgs.packages.${system};
 	in
 	{
 		nixosConfigurations.default = nixpkgs.lib.nixosSystem {
-			inherit system;
-			pkgs = nixpkgs;
+			inherit pkgs system;
 			modules = [
 				./hosts/default
 				/etc/nixos/hardware-configuration.nix
 			];
 		};
 		homeConfigurations.qow = home-manager.lib.homeManagerConfiguration {
-			inherit system;
+			inherit pkgs system;
 			modules = [import ./users/qow];
 		};
 	};
