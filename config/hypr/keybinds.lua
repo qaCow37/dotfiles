ffa292dce98b94704278b1d52efd556769616516
qaCow37 <daily.cow37@proton.me>
2026-06-04 14:12:01 +0000

    added kitty keybind

--- config/hypr/keybinds.lua
+++ config/hypr/keybinds.lua
@@ -1,6 +1,7 @@
 local mod = {}
 
 function mod.conf()
+	hl.bind("CTRL + ALT + T"  , hl.dsp.exec("kitty"))
 	hl.bind("ALT         + F4", hl.dsp.window.close())
 	hl.bind("ALT + SHIFT + F4", hl.dsp.window.kill())
 end
