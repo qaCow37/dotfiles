6a782408184db52ee1f076a44d3ea54a7663b330
qaCow37 <daily.cow37@proton.me>
2026-06-04 14:47:31 +0000

    added basic window control

--- config/hypr/keybinds.lua
+++ config/hypr/keybinds.lua
@@ -1,9 +1,64 @@
 local mod = {}
 
+function mod.overlap_guard()
+	local n = false
+	return function(dsp, default)
+		if default == true then
+			return function()
+				if n == false then
+					hl.dispatch(dsp)
+				end
+				n = false
+			end
+		else
+			return function()
+				hl.dispatch(dsp)
+				n = true
+			end
+		end
+	end
+end
+
 function mod.conf()
-	hl.bind("CTRL + ALT + T"  , hl.dsp.exec("kitty"))
-	hl.bind("ALT         + F4", hl.dsp.window.close())
-	hl.bind("ALT + SHIFT + F4", hl.dsp.window.kill())
+	hl.bind("SUPER_L", hl.dsp.submap("overview"))
+	hl.define_submap("overview", function()
+		local guard = mod.overlap_guard()
+		hl.bind("SPACE + UP"   , guard(hl.dsp.window.move({direction="u"})))
+		hl.bind("SPACE + LEFT" , guard(hl.dsp.window.move({direction="l"})))
+		hl.bind("SPACE + DOWN" , guard(hl.dsp.window.move({direction="d"})))
+		hl.bind("SPACE + RIGHT", guard(hl.dsp.window.move({direction="r"})))
+		hl.bind("UP"           , guard(hl.dsp.focus({direction="u"}), true))
+		hl.bind("LEFT"         , guard(hl.dsp.focus({direction="l"}), true))
+		hl.bind("DOWN"         , guard(hl.dsp.focus({direction="d"}), true))
+		hl.bind("RIGHT"        , guard(hl.dsp.focus({direction="r"}), true))
+		hl.bind("SHIFT + UP"   , hl.dsp.window.resize({x=  0,y=-50,relative=true}), {repeating=true})
+		hl.bind("SHIFT + LEFT" , hl.dsp.window.resize({x=-50,y=  0,relative=true}), {repeating=true})
+		hl.bind("SHIFT + DOWN" , hl.dsp.window.resize({x=  0,y= 50,relative=true}), {repeating=true})
+		hl.bind("SHIFT + RIGHT", hl.dsp.window.resize({x= 50,y=  0,relative=true}), {repeating=true})
+
+		hl.bind("SPACE"  , hl.dsp.no_op())
+		hl.bind("SHIFT_L", hl.dsp.no_op())
+		hl.bind("SHIFT_R", hl.dsp.no_op())
+
+		local function reset()
+			hl.dispatch(hl.dsp.submap("reset"))
+		end
+
+		local non_consuming = {non_consuming=true}
+		hl.bind("ESCAPE"   , reset)
+		hl.bind("RETURN"   , reset)
+		hl.bind("catchall" , reset, non_consuming)
+		hl.bind("mouse:272", reset, non_consuming)
+		hl.bind("mouse:273", reset, non_consuming)
+		hl.bind("mouse:274", reset, non_consuming)
+		hl.bind("mouse:275", reset, non_consuming)
+		hl.bind("mouse:276", reset, non_consuming)
+	end)
+
+	local universal = {submap_universal=true}
+	hl.bind("CTRL + ALT + T"  , hl.dsp.exec_cmd("kitty"), universal)
+	hl.bind("ALT         + F4", hl.dsp.window.close()   , universal)
+	hl.bind("ALT + SHIFT + F4", hl.dsp.window.kill()    , universal)
 end
 
 return mod
